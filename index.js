50dff68a47303436b43ed11660a63352103bae62
AhmadArabati <68800819+AhmadArabati@users.noreply.github.com>
2025-02-07 07:13:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const path = require('path');
 const handlebars = require('express-handlebars');
 const session = require('express-session');
 const flash = require('connect-flash');
-const https = require('http'); // https
+const https = require('http');
 const fs = require('fs');
 const WebSocket = require("ws");
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS); // When upload the project on https add key name it GOOGLE_APPLICATION_CREDENTIALS in env // google will disable any public key so in env will be a secret file
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 // const serviceAccount = require('./credentials.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -115,12 +115,13 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set("trust proxy", 1);
 app.use(
     session({
-        secret: process.env.SESSION_SECRET, // if https process.env.SESSION_SECRET
+        secret: process.env.SESSION_SECRET,
         resave: false,
-        saveUninitialized: false, // false
-        cookie: { secure: true, httpOnly: true, sameSite: 'strict', maxAge: 1000 * 60 * 30 }, // true if https cookie: { secure: true, httpOnly: true, sameSite: 'strict', maxAge: 1000 * 60 * 30 }
+        saveUninitialized: false,
+        cookie: { secure: true, httpOnly: true, sameSite: 'strict', maxAge: 1000 * 60 * 30 },
     })
 );
 
