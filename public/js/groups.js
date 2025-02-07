d55d027dbb577fc769bd9ba19cf103b01c7cb8d8
AhmadArabati <68800819+AhmadArabati@users.noreply.github.com>
2025-02-07 16:04:45 +0300

    Add files via upload

---
+++ public/js/groups.js
@@ -0,0 +1,68 @@
+const groups = [
+    {
+        name: 'BAU Zarqa',
+        link: 'https://chat.whatsapp.com/L4KJ6jiyKN0IPH3lT2pCmO'
+    },
+    {
+        name: 'Medical Analysis 🔬',
+        link: 'https://chat.whatsapp.com/DcKBA6FhohLJdCZrjlXSsT'
+    },
+    {
+        name: 'كلية الزرقاء الجامعية (٢) 📚🖊️',
+        link: 'https://chat.whatsapp.com/JpP6g3fBX7JH0jtN3S9h9L'
+    },
+    {
+        name: 'طلاب التمريض/كلية الزرقاء الجامعية',
+        link: 'https://chat.whatsapp.com/L9mVzxNoayAGEdUPwMbsFQ',
+        image: 'طلاب التمريض كلية الزرقاء الجامعية'
+    },
+    {
+        name: 'تخصص التمريض دبلوم 🩺💉',
+        link: 'https://chat.whatsapp.com/JfBNEStueSy7AZbs7Nl07l'
+    },
+    {
+        name: 'الطفيليات الطبية // الفصل الثاني',
+        link: 'https://chat.whatsapp.com/D5Y6EJO5ckBFWh2kTyWBtz',
+        image: 'الطفيليات الطبية الفصل الثاني'
+    },
+    {
+        name: 'دم (2)',
+        link: 'https://chat.whatsapp.com/Kb9Oo6XzAbLJcBUXInrlNY'
+    },
+    {
+        name: 'غدد',
+        link: 'https://chat.whatsapp.com/ERo7Besr46NFK8L6O8oVl9'
+    },
+    {
+        name: 'انجليزي (2)',
+        link: 'https://chat.whatsapp.com/FbcGi8kPisSJIqjnI6WQR5'
+    },
+    {
+        name: 'عسكرية',
+        link: 'https://chat.whatsapp.com/HdnTTdxtF5n7HrTR3b1Tln'
+    },
+    {
+        name: 'انجليزي ١',
+        link: 'https://chat.whatsapp.com/BuJQuvg7xvW6st4NisFXzo'
+    },
+    {
+        name: 'تشخيصية',
+        link: 'https://chat.whatsapp.com/KApBOE9zQIqBHRHeGMPujm'
+    },
+    {
+        name: 'حاسوب ٢',
+        link: 'https://chat.whatsapp.com/EMriHNsTg8E08Nie9jLZFj'
+    },
+    {
+        name: 'بنك دم',
+        link: 'https://chat.whatsapp.com/CZ16Zjxq97fCk0VpqXXuEz'
+    },
+    {
+        name: 'مايكرو طبية (الدكتور معاذ)',
+        link: 'https://chat.whatsapp.com/KieB6pbKCocBJbXu0ig3hJ'
+    },
+    // {
+    //     name: 'BAU Zarqa',
+    //     link: 'https://chat.whatsapp.com/L4KJ6jiyKN0IPH3lT2pCmO'
+    // },
+];
