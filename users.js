56623dc7c87d1f14353e6fc59d0219a909eae2ce
AhmadArabati <68800819+AhmadArabati@users.noreply.github.com>
2025-02-20 01:19:37 +0300

    Add files via upload

---
+++ users.js
@@ -0,0 +1,281 @@
+const users = [
+  {
+    id: 'zkpFDkPUcB3r5YYWXTqa',
+    name: 'Raghad Ra’ed',
+    uniId: '32217030095',
+    phone: '0788764068',
+    password: '$2a$10$tT9sGFUVyw5TSgN/Xwn2HeACAooW8LsHLp9nYIsqRaXapBJTTW63e',
+    createdAt: Timestamp { _seconds: 1739998566, _nanoseconds: 354000000 },
+    verified: true
+  },
+  {
+    id: 'oTKGfxQoR080FUgYJ00E',
+    name: 'Batool osama',
+    uniId: '32217036040',
+    phone: '0791806102',
+    password: '$2a$10$6DxKckLkS14/RKprdt4liOshv9Iqd/7QsGL/EOHyaHye4P3O98OU6',
+    createdAt: Timestamp { _seconds: 1739996716, _nanoseconds: 188000000 },
+    verified: true
+  },
+  {
+    id: '3cqhGqcLTZN7kUfvD5rl',
+    name: 'Raghad Hassan',
+    uniId: '32217043223',
+    phone: '0790907883',
+    password: '$2a$10$Kl2VqjZrcZVvABTIwqQJ0.FNGTAzr6tdSwYoAawUAbBfz3TZsL3QW',
+    createdAt: Timestamp { _seconds: 1739995708, _nanoseconds: 358000000 },
+    verified: true
+  },
+  {
+    id: '2GA5qIqbzlhoTVCk1y4r',
+    name: 'Sondos Alnatour',
+    uniId: '32217030022',
+    phone: '0775545201',
+    password: '$2a$10$YdqKx2cQpQc1NaXrcvMKl.KDVaywTE4aMVJ2PNQMpkmAYu0DIcp5e',
+    createdAt: Timestamp { _seconds: 1739994895, _nanoseconds: 227000000 },
+    verified: true
+  },
+  {
+    id: '3GbdAOJoPbMoa6ATKBBt',
+    uniId: '32217030050',
+    phone: '0785330572',
+    password: '$2a$10$09ogS1.C1MtbJNMrNLPibupQ0QimJi070DoIrn2n6vJ4ebuiWhaVm',
+    createdAt: Timestamp { _seconds: 1739665198, _nanoseconds: 569000000 },
+    verified: true,
+    name: 'Ahmad Arabati'
+  },
+  {
+    id: 'VLydOuJcgyleFfGW962j',
+    name: 'Abdelsatar Abdo',
+    uniId: '32217036081',
+    phone: '0782561411',
+    password: '$2a$10$CssvuOedbI0WICh.NIRm8uyQ8rIwzbccLDWiLbieEZtvD7iNpRjQy',
+    createdAt: Timestamp { _seconds: 1739461097, _nanoseconds: 461000000 },
+    verified: true
+  },
+  {
+    id: 'PkfvYfWSmnltz3ewa5mO',
+    name: 'Besan.almasaeed',
+    uniId: '32217030128',
+    phone: '0776318908',
+    password: '$2a$10$kNS5FRX1GdPoJb1hq8PumezjlyTJViLF8svZFX/dm89yMJoiC6jje',
+    createdAt: Timestamp { _seconds: 1738957063, _nanoseconds: 969000000 },
+    verified: true
+  },
+  {
+    id: 'AlYCNzlnWE9HgkbAbHMO',
+    name: 'maha',
+    uniId: '32317043007',
+    phone: '0797365163',
+    password: '$2a$10$pKC48CjAgIDaEz5Mxdxpiu.GVSZidIPxOYmUx/HcSNdvp10kbbwdW',
+    createdAt: Timestamp { _seconds: 1738956254, _nanoseconds: 338000000 },
+    verified: true
+  },
+  {
+    id: '7d6qXaNEx9qhsG23hnmj',
+    name: 'Ayat',
+    uniId: '32117043031',
+    phone: '0797713281',
+    password: '$2a$10$qzZYIU/OXEC.bB7ZOiW84eGrgtAAymrWKRvinwABBVyu7YDPLH6ie',
+    createdAt: Timestamp { _seconds: 1738951148, _nanoseconds: 585000000 },
+    verified: true
+  },
+  {
+    id: 'CnXb3zmabgfyJ10Zc1Vx',
+    name: '𝓡𝓜𝓕'   ,
+    uniId: '32217043080',
+    phone: '0780146106',
+    password: '$2a$10$Mjrkqy8Ry40oA3Cuu.7TTe20SYz32lo/SrtST6N9sJnL3qMILvvxG',
+    createdAt: Timestamp { _seconds: 1738949457, _nanoseconds: 850000000 },
+    verified: true
+  },
+  {
+    id: 'EJxAMLvsV5g0aqd9qsqs',
+    name: 'Baraa awwad',
+    uniId: '32417043015',
+    phone: '0788395371',
+    password: '$2a$10$H1Lk0ibOc11RakyUoTLru.E.l6XCQ5crnGDPnzt1clY/PgBiOC1ci',
+    createdAt: Timestamp { _seconds: 1738940248, _nanoseconds: 623000000 },
+    verified: true
+  },
+  {
+    id: 'XcTmD8lvk2TGLhP8iz2h',
+    name: 'Amro AlSmadi',
+    uniId: 'test',
+    phone: 'test',
+    password: '$2a$10$RXc0Pc7oBVygLrBslXxV8.ZzhovCzevGTuquLkqRWWmT0sBLLwllu',
+    createdAt: Timestamp { _seconds: 1738895234, _nanoseconds: 283000000 },
+    verified: true
+  },
+  {
+    id: '0r1Wk1ee5fFGeuObPiDu',
+    uniId: '32217030131',
+    phone: '0779114372',
+    password: '$2a$10$Jf365IJ8C6CCPs94QJJ7OOb7wHgsKgpIdBBKBBWc1VFtfWGVF8Kn.',
+    createdAt: Timestamp { _seconds: 1738863416, _nanoseconds: 505000000 },
+    verified: true,
+    name: 'Ali Alsarhan'
+  },
+  {
+    id: 'WHgc9xYqjqIC49rhNxWj',
+    name: 'ميس  العنانبه',
+    uniId: '32217030156',
+    phone: '0776511924',
+    password: '$2a$10$m2On3ydSmq.dyQJ5OLaLW.jK9w9jBc62S7HFZo/IAJHOuEgiqS4Du',
+    createdAt: Timestamp { _seconds: 1738860761, _nanoseconds: 776000000 },
+    verified: true
+  },
+  {
+    id: 'pHVOUSkqysp3JYDFDZvs',
+    name: 'باسمه شاهر ابو قويدر',
+    uniId: '32217030023',
+    phone: '0785206432',
+    password: '$2a$10$ngNyDiUunuUCB6bXUCpcRO77eiL89gH0796HUHNRZb.xQ8g.yhyLu',
+    createdAt: Timestamp { _seconds: 1738855111, _nanoseconds: 367000000 },
+    verified: true
+  },
+  {
+    id: 'pVdHn4nG6jv50Py7i0ef',
+    name: 'بيسان عوض',
+    uniId: '32217043220',
+    phone: '0796504673',
+    password: '$2a$10$3TwY77ECCX6UDiR.3ojP6.rcJUWYehxY5zqfYfW2Ipzm/xUE.qZd.',
+    createdAt: Timestamp { _seconds: 1738837614, _nanoseconds: 371000000 },
+    verified: true
+  },
+  {
+    id: 'ngPqWt9yzTHp2MPdVm4N',
+    name: 'Jana Alqudah',
+    uniId: '32217030137',
+    phone: '0778632020',
+    createdAt: Timestamp { _seconds: 1738786455, _nanoseconds: 860000000 },
+    verified: true,
+    password: '$2a$10$4QetKUXK9uENl6y7QV82GuuiQ.euDYUDgki3OJCZhGRJ9s.sxWkaa'
+  },
+  {
+    id: 'jshPPKvWJTlA0Cf6fg12',
+    name: 'Eman Zghoul ',
+    uniId: '22417099008',
+    phone: '0776457086',
+    password: '$2a$10$IUHYXCGQOpE88PfZRsDzkeeJCvscZV7AftpGaHxoDoCC1umtVpRVe',
+    createdAt: Timestamp { _seconds: 1738778526, _nanoseconds: 942000000 },
+    verified: true
+  },
+  {
+    id: 'kjc5xWPam2Q4Br4C81IA',
+    name: 'حسام الحاج',
+    uniId: '32417030024',
+    phone: '0786214609',
+    password: '$2a$10$HTrFqYvKBZtCdXon6QSOmuo19ZLkjA0qnTjkdE7QU0A7ykKC3PrpS',
+    createdAt: Timestamp { _seconds: 1738778097, _nanoseconds: 177000000 },
+    verified: true
+  },
+  {
+    id: '8hhj5Zuto4NaxQAyhta9',
+    name: 'Tasbeeh qtishat',
+    uniId: '32217030159',
+    phone: '0795863449',
+    password: '$2a$10$6sCgpyagmVe6moilAolVruzvO4tH2ofQpd0pgfzKXO4hQUybdqBCW',
+    createdAt: Timestamp { _seconds: 1738774738, _nanoseconds: 801000000 },
+    verified: true
+  },
+  {
+    id: 'CW2ot75wgsoUP6sQsVkE',
+    name: 'Shahdfahmyabuyehyeh ',
+    uniId: '32217043136',
+    phone: '0782147041',
+    password: '$2a$10$/1JOInpl4rOY6lizA4NDcuPAT0XtmxoXVWcXQ3k/p5y3tSJ9I6pWK',
+    createdAt: Timestamp { _seconds: 1738774486, _nanoseconds: 160000000 },
+    verified: true
+  },
+  {
+    id: '6BDtFxAKSXPmV7iX7gj3',
+    name: 'Shrooq albsharat',
+    uniId: '32217043198',
+    phone: '0788631344',
+    password: '$2a$10$l/SeA7i1FsRTyQ4RQkAdmubh6T0hpXsgsKWr1EOeCMBjKM2zyF0Ay',
+    createdAt: Timestamp { _seconds: 1738774000, _nanoseconds: 914000000 },
+    verified: true
+  },
+  {
+    id: 'rd15Z8kchu4Yl0cFxLir',
+    name: 'Amro Smadi',
+    uniId: '32217030157',
+    phone: '0777340123',
+    password: '$2a$10$iI62xIxfdv6crUzqbvORieQCQWco7gK1.0XzrxC4GimqSBGLB30CC',
+    createdAt: Timestamp { _seconds: 1738772929, _nanoseconds: 669000000 },
+    verified: true
+  },
+  {
+    id: '0KqJKq97951QxQgMVczb',
+    name: 'Almuthana Albuhisi',
+    uniId: '32317030512',
+    phone: '0782362085',
+    password: '$2a$10$z.0t.HU5cja0DY1bU90n9e2NTVZcOOlSd8uPJQHttVKRsoaRm8rRK',
+    createdAt: Timestamp { _seconds: 1738772818, _nanoseconds: 543000000 },
+    verified: true
+  },
+  {
+    id: 'nqNGSUQf3dqcQmn9wVuk',
+    name: 'Ayat Maqableh ',
+    uniId: '22417251007',
+    phone: '0775372741',
+    password: '$2a$10$pV1B4f8dYSrWYcFpEl7qRehI2/cQr6fstZdV1ndhXBTDEyFHqUaOq',
+    createdAt: Timestamp { _seconds: 1738772173, _nanoseconds: 16000000 },
+    verified: true
+  },
+  {
+    id: 'D35TAbjcakOZjjfC7MsZ',
+    name: 'Batool Hussein ',
+    uniId: '32217030042',
+    phone: '0787909654',
+    password: '$2a$10$kB1JCh5kkWNIjgI/l9CGv.83Gdtju.bWPi4KruWthoibep7fwOkMi',
+    createdAt: Timestamp { _seconds: 1738772171, _nanoseconds: 542000000 },
+    verified: true
+  },
+  {
+    id: 'fQDxMV7jzHV54QoWezY9',
+    name: 'Farah Sarhan',
+    uniId: '32217043175',
+    phone: '0780756075',
+    password: '$2a$10$3Y2xb49vVDxre4RzyQcOpum8.8efuRGUiXlB2RcWoq9URyfsf2LAm',
+    createdAt: Timestamp { _seconds: 1738772069, _nanoseconds: 991000000 },
+    verified: true
+  },
+  {
+    id: '87mjWbAr57qlb4fm5CUE',
+    name: 'Malk ismael',
+    uniId: '32217043158',
+    phone: '0782408530',
+    password: '$2a$10$kirdi/xaqyNdz0Wfn8Tblu3q6aM7GyBZqjCvXwDfcExSqnJzWfCjC',
+    createdAt: Timestamp { _seconds: 1738772016, _nanoseconds: 806000000 },
+    verified: true
+  },
+  {
+    id: 'cD6kLvzOdwVielskGaks',
+    name: 'Zainab khawaldeh',
+    uniId: '32217030035',
+    phone: '0775853945',
+    password: '$2a$10$T.fqpee1HpdVOeyAihLcfeO7vSJuAqIAREMbZCqtQ5mUyX3ySSyOO',
+    createdAt: Timestamp { _seconds: 1738771446, _nanoseconds: 441000000 },
+    verified: true
+  },
+  {
+    id: 'd7FzlPcdCcqovpuPI4dE',
+    name: 'Asal Banimustafa ',
+    uniId: '22217082341',
+    phone: '0772605754',
+    password: '$2a$10$KCM3x248JEHrfamXyUW2yefXhWEpiDmlXpXVqxNs1eNQ7Hx6pD6nq',
+    createdAt: Timestamp { _seconds: 1738769971, _nanoseconds: 589000000 },
+    verified: true
+  },
+  {
+    id: 'gtjukscywz0DdvpLtcUo',
+    name: 'Aseel Aladwan',
+    uniId: '32317030072',
+    phone: '0788204488',
+    password: '$2a$10$HZc0RUGiYueZNEztF4xbcuOVvSqaBxUBU8qjxJHvBdlwn7H39uC0q',
+    createdAt: Timestamp { _seconds: 1738769433, _nanoseconds: 426000000 },
+    verified: true
+  }
+]
